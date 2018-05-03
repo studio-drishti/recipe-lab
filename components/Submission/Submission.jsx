733d17d082780509af352dbcb36cfa38121f02ef
Jay Nielsen <jayarnielsen@gmail.com>
2018-05-02 21:43:46 -0600

    stubbed in some forms

--- components/Submission/Submission.jsx
+++ components/Submission/Submission.jsx
@@ -3,6 +3,23 @@ import css from './Submission.css'
 
 export default () => (
   <form className={css.form}>
+    <fieldset className={css.primaryFieldset}>
+      <label>
+        Title
+        <input type="text" />
+      </label>
 
+      <label>
+        Description
+        <textarea></textarea>
+      </label>
+
+    </fieldset>
+    <fieldset className={css.secondaryFieldset}>
+      <label>
+        Additional Details
+        <textarea></textarea>
+      </label>
+    </fieldset>
   </form>
 )
