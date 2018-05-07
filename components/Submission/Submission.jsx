e540f4c566f83d94a4b0fc280e5582a0ddce7e99
Jay Nielsen <jayarnielsen@gmail.com>
2018-05-07 16:04:37 -0600

    initial dummy seeds

--- components/Submission/Submission.jsx
+++ components/Submission/Submission.jsx
@@ -1,25 +1,89 @@
 import Link from 'next/link'
 import css from './Submission.css'
+import { Component } from 'react'
 
-export default () => (
-  <form className={css.form}>
-    <fieldset className={css.primaryFieldset}>
-      <label>
-        Title
-        <input type="text" />
-      </label>
+class Submission extends Component {
+  state = {
+    title: '',
+    description: '',
+    currentIngredient: 0,
+    ingredients: [
+      {
+        ingredient: '',
+        detail: '',
+      },
+    ]
+  }
 
-      <label>
-        Description
-        <textarea></textarea>
-      </label>
+  addIngredient = () => {
+    const { ingredients } = this.state
+    ingredients.push({
+      ingredient: '',
+      detail: ''
+    })
+    this.setState({ingredients})
+  }
 
-    </fieldset>
-    <fieldset className={css.secondaryFieldset}>
-      <label>
-        Additional Details
-        <textarea></textarea>
-      </label>
-    </fieldset>
-  </form>
-)
+  showIngredientDetails = (i) => {
+    this.setState({currentIngredient: i})
+  }
+
+  handleIngredientChange = (e) => {
+    const { ingredients } = this.state;
+    const i = e.target.getAttribute('data-index')
+    ingredients[i].ingredient = e.target.value;
+    this.setState({ingredients})
+  }
+
+  handleIngredientDetailChange = (e) => {
+    const { ingredients } = this.state
+    const i = e.target.getAttribute('data-index')
+    ingredients[i].detail = e.target.value;
+    this.setState({ingredients})
+  }
+
+  render() {
+    const { ingredients, currentIngredient } = this.state;
+    return (
+      <form className={css.form}>
+        <fieldset className={css.primaryFieldset}>
+          <label>
+            Title
+            <input type="text" />
+          </label>
+
+          <label>
+            Description
+            <textarea></textarea>
+          </label>
+
+          <p>Ingredients</p>
+          {ingredients.map((ingredient, i) => (
+            <label key={i}>
+              <input
+                type="text"
+                placeholder={`Ingredient #${i}`}
+                data-index={i}
+                onChange={this.handleIngredientChange}
+                onFocus={() => this.showIngredientDetails(i)} />
+            </label>
+          ))}
+          <button type="button" onClick={this.addIngredient}>Add Ingredient</button>
+
+        </fieldset>
+        <fieldset className={css.secondaryFieldset}>
+          <label>
+            {`Additonal details about the ingredient: ${ingredients[currentIngredient].ingredient}`}
+            <textarea
+              value={ingredients[currentIngredient].detail}
+              data-index={currentIngredient}
+              onChange={this.handleIngredientDetailChange}
+            />
+          </label>
+        </fieldset>
+      </form>
+    )
+  }
+}
+
+export default Submission;
---
+++ util/seed.js
@@ -0,0 +1,101 @@
+export const recipes = [
+  {
+    id: 0,
+    name: 'Spaghetti and Meatballs',
+    author: 'The Curnielswensens',
+    source: 'Clean Eats',
+    prepTime: 900000, //15 minutes
+    cookTime: 2700000, //45 minutes
+    yields: {
+      quantity: 4,
+      unit: "servings"
+    },
+    tags: [
+      'Gluten Free',
+    ],
+    images: [
+      'https://media.giphy.com/media/ToMjGpyWnjWUZbfi7rq/giphy.gif',
+    ],
+    description: `It's spaghett! This super easy recipe is delcious,
+                  nutritious, and sure to be a crowd pleaser.
+                  Rice noodles done right are practically indistinguishable
+                  from their glutenfull counterparts.`,
+    type: 'main',
+    ingredients: [
+      {
+        name: 'gulten-free spaghetti',
+        quantity: 1,
+        unit: 'lb',
+        toTaste: false,
+        processing: '',
+        notes: '',
+      },
+      {
+        name: 'avocado oil',
+        quantity: 2,
+        unit: 'tbsp',
+        toTaste: false,
+        processing: '',
+        notes: 'plus another few tablespoons for cooking the meatballs',
+      },
+      {
+        name: 'medium onion',
+        quantity: 1,
+        unit: '',
+        toTaste: false,
+        processing: 'chopped',
+        notes: 'Preferablly red, but brown onions work just as well.',
+      },
+      {
+        name: 'garlic cloves',
+        quantity: 2,
+        unit: '',
+        toTaste: false,
+        processing: 'minced',
+        notes: '',
+      },
+      {
+        name: 'italian seasoning',
+        quantity: 2,
+        unit: 'tsp',
+        toTaste: false,
+        processing: '',
+        notes: '',
+      },
+      {
+        name: 'red wine',
+        quantity: 2,
+        unit: 'cup',
+        toTaste: false,
+        processing: '',
+        notes: '',
+      },
+      {
+        name: 'chicken stock',
+        quantity: 0.5,
+        unit: 'cup',
+        toTaste: false,
+        processing: '',
+        notes: '',
+      },
+      {
+        name: '28-ounce can whole peeled tomatoes',
+        quantity: 1,
+        unit: '',
+        toTaste: false,
+        processing: 'blended into a puree',
+        notes: '',
+      },
+    ],
+    steps: [
+      {
+        directions: 'Preheat the oven to 350 F',
+        notes: '',
+      },
+      {
+        directions: `Heat a large pot over medium-high heat. Add 2 tablespoons of the avocado oil, and when it's warm, saute the onion until it's brown and translucent`,
+        notes: 'Try to cook the onions longer than you think will be necessary. Get them real carmelized. Yum Yum.',
+      },
+    ],
+  },
+]
