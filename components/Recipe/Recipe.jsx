b698f7c452339743d3bc56b2f229b82b078c4ad6
Jay Nielsen <jayarnielsen@gmail.com>
2018-05-11 14:57:19 -0600

    started implementing recipe mods

--- components/Recipe/Recipe.jsx
+++ components/Recipe/Recipe.jsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import { Component } from 'react'
+import Textarea from "react-textarea-autosize";
+import merge from 'deepmerge';
 
 import css from './Recipe.css'
 import { stepsToIngredientTotals, formatIngredientTotal } from '../../util/recipeTools';
@@ -8,10 +10,46 @@ class Recipe extends Component {
   state = {
     currentStep: 0,
     recipe: this.props.recipe,
+    editing: false,
+  }
+
+  // componentDidMount() {
+  //   if(localStorage.getItem('recipeMods') !== null) {
+  //     const mods = JSON.parse(localStorage.getItem('recipeMods'));
+  //     let { recipe } = this.state
+  //     console.log(mods)
+  //     recipe = merge(recipe, mods);
+  //     this.setState({ recipe })
+  //   }
+  // }
+
+  toggleEdit = (e) => {
+    if(this.state.editing === false) {
+      this.setState({editing: true})
+    } else {
+      this.setState({editing: false})
+    }
+  }
+
+  handleStepChange = (e) => {
+    const { name, value } = e.target
+    const { recipe } = this.state
+    recipe.steps[this.state.currentStep][name] = value
+
+
+    // localStorage.setItem('recipeMods', JSON.stringify({
+    //   steps: {
+    //     [this.state.currentStep]: {
+    //       [name]: value
+    //     }
+    //   }
+    // }))
+
+    this.setState({recipe})
   }
 
   render() {
-    const { recipe, currentStep } = this.state;
+    const { recipe, currentStep, editing } = this.state;
     return (
       <article className={css.recipe}>
         <div className={css.recipeMain}>
@@ -34,8 +72,17 @@ class Recipe extends Component {
           </ol>
         </div>
         <div className={css.recipeDetail}>
-            {recipe.steps[currentStep].ingredients.length > 0 &&
+            <div className={css.recipeActions}>
+              <button onClick={this.toggleEdit}>
+                <i className="material-icons">edit</i>
+              </button>
+              <button>
+                <i className="material-icons">delete</i>
+              </button>
+            </div>
+            {recipe.steps[currentStep].ingredients.length > 0 && (
               <div>
+
                 <h3>Ingredients Used</h3>
                 <ul>
                   {recipe.steps[currentStep].ingredients.map((ingredient, i) => (
@@ -47,15 +94,31 @@ class Recipe extends Component {
                 </ul>
 
               </div>
-            }
-            <p>
-              {recipe.steps[currentStep].directions}
-            </p>
-            {recipe.steps[currentStep].notes &&
+            )}
+
+            {editing ? (
+              <Textarea
+                name="directions"
+                value={recipe.steps[currentStep].directions}
+                placeholder="Directions"
+                onChange={this.handleStepChange} />
+            ) : (
+              <p>
+                {recipe.steps[currentStep].directions}
+              </p>
+            )}
+
+            {editing ? (
+              <Textarea
+                name="notes"
+                value={recipe.steps[currentStep].notes}
+                placeholder="Additional Notes"
+                onChange={this.handleStepChange} />
+            ) : (
               <p>
                 {recipe.steps[currentStep].notes}
               </p>
-            }
+            )}
         </div>
       </article>
     )
