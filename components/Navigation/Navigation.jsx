23239cbced9e580d62f7309655c9bce53e30b978
Jay Nielsen <jayarnielsen@gmail.com>
2018-05-16 08:23:14 -0600

    corrected link usage

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -6,9 +6,21 @@ export default () => (
     <Link href="/">
       <a className={css.logo}>Schooled Lunch</a>
     </Link>
-    <Link href="/recipes">Recipes</Link>
-    <Link href="/about">About</Link>
-    <Link href="/submit">Submit</Link>
+    <Link href="/recipes">
+      <a>
+        Recipes
+      </a>
+    </Link>
+    <Link href="/about">
+      <a>
+        About
+      </a>
+    </Link>
+    <Link href="/submit">
+      <a>
+        Submit
+      </a>
+    </Link>
     <input type="search" placeholder="Search..." />
   </nav>
 )
