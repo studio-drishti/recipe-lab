bb207b836411059463fd091964cac3c2f7da2520
Jay Nielsen <jayarnielsen@gmail.com>
2018-12-18 15:37:16 -0700

    refactored item list as classes

--- client/components/Item/Item.jsx
+++ client/components/Item/Item.jsx
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { Draggable } from 'react-beautiful-dnd';
 import { MdDragHandle } from 'react-icons/md';
 
 import css from './Item.css';
 
-const Item = ({ children, item, index }) => (
-  <Draggable type="ITEM" draggableId={item._id} index={index}>
-    {provided => (
-      <div
-        className={css.item}
-        ref={provided.innerRef}
-        {...provided.draggableProps}
-      >
-        <div {...provided.dragHandleProps}>
-          <h3>
-            <i>
-              <MdDragHandle />
-            </i>
-            Directions for {item.name}
-          </h3>
-        </div>
-        {children}
-      </div>
-    )}
-  </Draggable>
-);
+export default class Item extends PureComponent {
+  render() {
+    const { children, item, index } = this.props;
+    return (
+      <Draggable type="ITEM" draggableId={item._id} index={index}>
+        {provided => (
+          <div
+            className={css.item}
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+          >
+            <div {...provided.dragHandleProps}>
+              <h3>
+                <i>
+                  <MdDragHandle />
+                </i>
+                Directions for {item.name}
+              </h3>
+            </div>
+            {children}
+          </div>
+        )}
+      </Draggable>
+    );
+  }
+}
 
 Item.displayName = 'Item';
-export default Item;
+Item.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number,
+  item: PropTypes.object
+};
--- client/components/ItemList/ItemList.jsx
+++ client/components/ItemList/ItemList.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Droppable } from 'react-beautiful-dnd';
 
 import css from './ItemList.css';
 
-const ItemList = ({ children, recipeId }) => (
-  <Droppable type="ITEM" droppableId={recipeId}>
-    {provided => (
-      <div
-        className={css.steps}
-        ref={provided.innerRef}
-        {...provided.droppableProps}
-      >
-        {children}
-        {provided.placeholder}
-      </div>
-    )}
-  </Droppable>
-);
+export default class ItemList extends Component {
+  render() {
+    const { children, recipeId } = this.props;
+    return (
+      <Droppable type="ITEM" droppableId={recipeId}>
+        {provided => (
+          <div
+            className={css.steps}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {children}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    );
+  }
+}
 
 ItemList.displayName = 'ItemList';
-export default ItemList;
+ItemList.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+  recipeId: PropTypes.string
+};
