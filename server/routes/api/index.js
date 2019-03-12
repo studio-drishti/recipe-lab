acd75996f2e9efd4d16661ae937956717d1c11a7
Jay Nielsen <jayarnielsen@gmail.com>
2019-03-12 11:37:00 -0600

    removed unused api endpoint

--- server/routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const avatarRoutes = require('./avatars');
-
-// Recipe routes
-router.use('/avatars', avatarRoutes);
-
-module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,5 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
 const healthzRoute = require('./healthz');
-
-// API Routes
-router.use('/api', apiRoutes);
 
 // Healthz
 router.use('/healthz', healthzRoute);
