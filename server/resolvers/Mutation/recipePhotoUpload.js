4905772bd98a955ad43555cf9715c48ccf02093b
Jay Nielsen <jayarnielsen@gmail.com>
2019-04-27 09:32:07 -0600

    fixed linter errors

--- server/resolvers/Mutation/recipePhotoUpload.js
+++ server/resolvers/Mutation/recipePhotoUpload.js
@@ -1,23 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const { storeFS } = require('../../utils/fileUploads');
-const getUserId = require('../../utils/getUserId');
 
 module.exports = async (parent, { file, recipeId }, ctx) => {
-  const userId = getUserId(ctx);
-
   // TODO: verify recipe exists or add a graphql shield rule that
   //       only lets owners post photos to own recipes
 
   const dest = path.resolve(__dirname, `../../public/recipes/${recipeId}`);
   fs.mkdirSync(dest, { recursive: true });
 
-  const {
-    createReadStream,
-    filename: originalFilename,
-    mimetype,
-    encoding
-  } = await file;
+  const { createReadStream, filename: originalFilename, mimetype } = await file;
 
   if (!['image/jpeg', 'image/jpg'].includes(mimetype))
     throw new Error('Invalid file type');
