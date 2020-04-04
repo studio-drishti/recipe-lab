59de35ecccb7f6d3b24c7f1328007fcc997f16f0
Jay Nielsen <jayarnielsen@gmail.com>
2020-04-04 11:02:41 -0600

    use default index when saving if no sort mod

--- server/resolvers/Mutation/publishRecipe.js
+++ server/resolvers/Mutation/publishRecipe.js
@@ -12,15 +12,15 @@ module.exports = async (parent, { recipeId }, ctx) => {
       servingAmount
       servingType
       description
-      items {
+      items(orderBy: index_ASC) {
         uid
         index
         name
-        steps {
+        steps(orderBy: index_ASC) {
           uid
           index
           directions
-          ingredients {
+          ingredients(orderBy: index_ASC) {
             uid
             index
             name
@@ -95,17 +95,16 @@ module.exports = async (parent, { recipeId }, ctx) => {
     return mod ? mod.value : source[fieldName];
   };
 
-  const maybeGetSortingIndex = (source, parentId) => {
+  const maybeGetSortingIndex = (source, parentId, index = 0) => {
     const sorting = sortings.find(sorting => sorting.parentId === parentId);
     if (sorting) return sorting.order.indexOf(source.uid);
-    if (source.index) return source.index;
-    return 0;
+    return index;
   };
 
-  const updateIngredients = (step, ingredient) => ({
+  const updateIngredients = (step, ingredient, i) => ({
     where: { uid: ingredient.uid },
     data: {
-      index: maybeGetSortingIndex(ingredient, step.uid),
+      index: maybeGetSortingIndex(ingredient, step.uid, i),
       name: maybeGetAlteration('name', ingredient),
       quantity: maybeGetAlteration('quantity', ingredient),
       unit: maybeGetAlteration('unit', ingredient),
@@ -113,64 +112,74 @@ module.exports = async (parent, { recipeId }, ctx) => {
     }
   });
 
-  const createIngredients = (step, ingredient) => ({
+  const createIngredients = (step, ingredient, i) => ({
     uid: ingredient.uid,
-    index: maybeGetSortingIndex(ingredient, step.uid),
+    index: maybeGetSortingIndex(ingredient, step.uid, i),
     name: ingredient.name,
     quantity: ingredient.quantity,
     unit: ingredient.unit,
     processing: ingredient.processing
   });
 
-  const updateSteps = (item, step) => ({
-    where: { uid: step.uid },
-    data: {
-      index: maybeGetSortingIndex(step, item.uid),
-      directions: maybeGetAlteration('directions', step),
-      ingredients: {
-        update: step.ingredients
-          .filter(ingredient => !removals.includes(ingredient.uid))
-          .map(ingredient => updateIngredients(step, ingredient)),
-        create: ingredientAdditions
-          .filter(addition => addition.parentId === step.uid)
-          .map(ingredient => createIngredients(step, ingredient))
+  const updateSteps = (item, step, i) => {
+    const ingredients = step.ingredients.filter(
+      ingredient => !removals.includes(ingredient.uid)
+    );
+    return {
+      where: { uid: step.uid },
+      data: {
+        index: maybeGetSortingIndex(step, item.uid, i),
+        directions: maybeGetAlteration('directions', step),
+        ingredients: {
+          update: ingredients.map((ingredient, i) =>
+            updateIngredients(step, ingredient, i)
+          ),
+          create: ingredientAdditions
+            .filter(addition => addition.parentId === step.uid)
+            .map((ingredient, i) =>
+              createIngredients(step, ingredient, i + ingredients.length)
+            )
+        }
       }
-    }
-  });
+    };
+  };
 
-  const createSteps = (item, step) => ({
+  const createSteps = (item, step, i) => ({
     uid: step.uid,
-    index: maybeGetSortingIndex(step, item.uid),
+    index: maybeGetSortingIndex(step, item.uid, i),
     directions: step.directions,
     ingredients: {
       create: ingredientAdditions
         .filter(addition => addition.parentId === step.uid)
-        .map(ingredient => createIngredients(step, ingredient))
+        .map((ingredient, i) => createIngredients(step, ingredient, i))
     }
   });
 
-  const updateItems = item => ({
-    where: { uid: item.uid },
-    data: {
-      index: maybeGetSortingIndex(item, recipe.uid),
-      name: maybeGetAlteration('name', item),
-      steps: {
-        update: item.steps.map(step => updateSteps(item, step)),
-        create: stepAdditions
-          .filter(addition => addition.parentId === item.uid)
-          .map(step => createSteps(item, step))
+  const updateItems = (item, i) => {
+    const steps = item.steps.filter(step => !removals.includes(step.uid));
+    return {
+      where: { uid: item.uid },
+      data: {
+        index: maybeGetSortingIndex(item, recipe.uid, i),
+        name: maybeGetAlteration('name', item),
+        steps: {
+          update: steps.map((step, i) => updateSteps(item, step, i)),
+          create: stepAdditions
+            .filter(addition => addition.parentId === item.uid)
+            .map((step, i) => createSteps(item, step, i + steps.length))
+        }
       }
-    }
-  });
+    };
+  };
 
-  const createItems = item => ({
+  const createItems = (item, i) => ({
     uid: item.uid,
     name: item.name,
-    index: maybeGetSortingIndex(item, recipe.uid),
+    index: maybeGetSortingIndex(item, recipe.uid, i),
     steps: {
       create: stepAdditions
         .filter(addition => addition.parentId === item.uid)
-        .map(step => createSteps(item, step))
+        .map((step, i) => createSteps(item, step, i))
     }
   });
 
@@ -183,6 +192,7 @@ module.exports = async (parent, { recipeId }, ctx) => {
     step: { item: { recipe: { id: recipe.id } } }
   });
 
+  const items = recipe.items.filter(item => !removals.includes(item.uid));
   const publishedRecipe = await ctx.prisma.updateRecipe({
     where: { uid: recipeId },
     data: {
@@ -195,10 +205,10 @@ module.exports = async (parent, { recipeId }, ctx) => {
         deleteMany: {
           uid_in: removals
         },
-        update: recipe.items
-          .filter(item => !removals.includes(item.uid))
-          .map(updateItems),
-        create: itemAdditions.map(createItems)
+        update: items.map(updateItems),
+        create: itemAdditions.map((item, i) =>
+          createItems(item, i + items.length)
+        )
       }
     }
   });
