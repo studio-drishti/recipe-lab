4eedce84e64c8306bdfb7de3eaba9d9c77cfed2b
Jay Nielsen <jayarnielsen@gmail.com>
2020-04-12 11:33:18 -0600

    silently create mods on login

--- client/components/Login/Login.jsx
+++ client/components/Login/Login.jsx
@@ -29,24 +29,55 @@ const Login = () => {
 
   const handleSubmission = e => {
     e.preventDefault();
-    signIn({
-      variables: fields
-    }).then(({ data }) => {
-      // Store the token in cookie
-      document.cookie = cookie.serialize('token', data.login.token, {
-        maxAge: 30 * 24 * 60 * 60 // 30 days
+    const variables = { ...fields };
+    const mods = Object.entries(localStorage)
+      .filter(([key]) => key.startsWith('MOD'))
+      .map(([key, value]) => {
+        const { sortings, alterations, removals, additions } = JSON.parse(
+          value
+        );
+        return {
+          recipeId: key.replace('MOD-', ''),
+          sortings,
+          alterations,
+          removals,
+          items: additions.filter(addition => addition.kind === 'Item'),
+          steps: additions.filter(addition => addition.kind === 'Step'),
+          ingredients: additions.filter(
+            addition => addition.kind === 'Ingredient'
+          )
+        };
       });
-      // Force a reload of all the current queries now that the user is
-      // logged in
-      client.cache
-        .reset()
-        .then(() => {
-          return refreshUser();
-        })
-        .then(user => {
-          redirect({}, `/chef/${user.slug}`);
+
+    if (mods.length) variables.modifications = mods;
+
+    signIn({ variables }).then(
+      ({
+        data: {
+          login: { token, recipeModsCreated }
+        }
+      }) => {
+        // Store the token in cookie
+        document.cookie = cookie.serialize('token', token, {
+          maxAge: 30 * 24 * 60 * 60 // 30 days
         });
-    });
+
+        // Remove any mods from localstorage that were created on the server
+        recipeModsCreated.forEach(recipeId => {
+          localStorage.removeItem(`MOD-${recipeId}`);
+        });
+
+        // Force a reload of all the current queries now that the user is logged in
+        client.cache
+          .reset()
+          .then(() => {
+            return refreshUser();
+          })
+          .then(user => {
+            redirect({}, `/chef/${user.slug}`);
+          });
+      }
+    );
   };
 
   return (
--- server/resolvers/Mutation/login.js
+++ server/resolvers/Mutation/login.js
@@ -1,18 +1,41 @@
 const { compare } = require('bcrypt');
 const { sign } = require('jsonwebtoken');
+const createModification = require('../../utils/createModification');
 
-module.exports = async (parent, { email, password }, ctx) => {
+module.exports = async (parent, { email, password, modifications }, ctx) => {
   const user = await ctx.prisma.user({ email });
-  console.log(user);
+  const recipeModsCreated = [];
+  const recipeModsInConflict = [];
   if (!user) {
     throw new Error(`No user found for email: ${email}`);
   }
+
   const passwordValid = await compare(password, user.password);
   if (!passwordValid) {
     throw new Error('Invalid password');
   }
+
+  await Promise.all(
+    modifications.map(async ({ recipeId, ...modification }) => {
+      const modExists = await ctx.prisma.$exists.modification({
+        user: { id: user.id },
+        recipe: { uid: recipeId }
+      });
+      if (!modExists) {
+        await createModification(ctx, recipeId, user.id, modification);
+        recipeModsCreated.push(recipeId);
+      } else {
+        recipeModsInConflict.push(recipeId);
+      }
+    })
+  );
+
+  const token = sign({ userId: user.id }, process.env.APP_SECRET);
+
   return {
-    token: sign({ userId: user.id }, process.env.APP_SECRET),
-    user
+    token,
+    user,
+    recipeModsCreated,
+    recipeModsInConflict
   };
 };
