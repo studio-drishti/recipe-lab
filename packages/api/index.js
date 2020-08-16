e9251d92bd2ccdcd3f46ac6096d655b5ed80626f
Jay Nielsen <jayarnielsen@gmail.com>
2020-08-16 02:40:13 +0000

    changed ready message

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,21 +1,21 @@
-const path = require('path');
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { loadSchemaSync } = require('@graphql-tools/load');
-const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader');
-const { addResolversToSchema } = require('@graphql-tools/schema');
-const morgan = require('morgan');
-const { applyMiddleware } = require('graphql-middleware');
-const { PrismaClient } = require('./generated/client');
-const resolvers = require('./resolvers');
-const permissions = require('./permissions');
+const path = require("path");
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { loadSchemaSync } = require("@graphql-tools/load");
+const { GraphQLFileLoader } = require("@graphql-tools/graphql-file-loader");
+const { addResolversToSchema } = require("@graphql-tools/schema");
+const morgan = require("morgan");
+const { applyMiddleware } = require("graphql-middleware");
+const { PrismaClient } = require("./generated/client");
+const resolvers = require("./resolvers");
+const permissions = require("./permissions");
 
 const prisma = new PrismaClient();
 
 const server = new ApolloServer({
   schema: applyMiddleware(
     addResolversToSchema(
-      loadSchemaSync(path.resolve(__dirname, 'schema/schema.graphql'), {
+      loadSchemaSync(path.resolve(__dirname, "schema/schema.graphql"), {
         loaders: [new GraphQLFileLoader()],
       }),
       resolvers
@@ -31,8 +31,8 @@ const server = new ApolloServer({
 });
 
 const app = express();
-app.use(morgan('common'));
-app.use('/public', express.static(path.resolve(__dirname, 'public')));
+app.use(morgan("common"));
+app.use("/public", express.static(path.resolve(__dirname, "public")));
 
 server.applyMiddleware({ app });
 
@@ -40,8 +40,8 @@ const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, (err) => {
   if (err) throw err;
-  if (process.env.NODE_ENV === 'development') {
-    console.info(`🍽  API Server Ready on http://localhost:${PORT}`);
+  if (process.env.NODE_ENV === "development") {
+    console.info(`ready - started api server on http://localhost:${PORT}`);
   } else {
     console.info(`API server is ready and listening on port ${PORT}`);
   }
