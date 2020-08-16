a06b73d728516d9df248b832e53c4ffa1d32435d
Jay Nielsen <jayarnielsen@gmail.com>
2020-08-16 14:02:26 +0000

    cleaned up bio

--- packages/web/components/RecipeBio/RecipeBio.jsx
+++ packages/web/components/RecipeBio/RecipeBio.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import { getChefBio } from '../../lib/chef';
-import css from './RecipeBio.module.css';
+import React from "react";
+import PropTypes from "prop-types";
+import { getChefBio } from "../../lib/chef";
+import css from "./RecipeBio.module.css";
 
 const RecipeBio = ({ author }) => (
   <div className={css.bio}>
-    <div>
+    <figure>
       <img src={author.avatar} />
-    </div>
+    </figure>
     <div>
       <h3>About {author.name}</h3>
       <p>{getChefBio(author)}</p>
     </div>
   </div>
 );
+
+RecipeBio.propTypes = {
+  author: PropTypes.object,
+};
 
 export default RecipeBio;
